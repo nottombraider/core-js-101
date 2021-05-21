d4baa63ca3791e0db3c7198775eaa6535f80f06b
nottombraider <larysa.yavorska@outlook.com>
2021-05-21 11:29:35 +0200

    feat: add solutions to arrays tasks part 2

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -576,8 +576,20 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((acc, item) => {
+    const itemKey = keySelector(item);
+    const itemValue = valueSelector(item);
+    const keyInAcc = acc.get(itemKey);
+
+    if (keyInAcc) {
+      keyInAcc.push(itemValue);
+    } else {
+      acc.set(itemKey, [itemValue]);
+    }
+
+    return acc;
+  }, new Map());
 }
 
 
@@ -594,8 +606,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((acc, item) => [...acc, ...childrenSelector(item)], []);
 }
 
 
@@ -611,8 +623,14 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, index, i) => {
+    if (i === 0) {
+      return arr[index];
+    }
+
+    return acc[index];
+  }, []);
 }
 
 
@@ -634,8 +652,15 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (!arr.length || arr.length === 1) return arr;
+
+  const isEven = (num) => num % 2 === 0;
+  const halfLength = Math.floor(arr.length / 2);
+  const head = arr.slice(0, halfLength);
+  const tail = isEven(arr.length) ? arr.slice(halfLength) : arr.slice(halfLength + 1);
+
+  return isEven(arr.length) ? [...tail, ...head] : [...tail, arr[halfLength], ...head];
 }
 
 
